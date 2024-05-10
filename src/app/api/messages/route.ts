4dd123f52f94adb4d746683c47b94add8c774d2f
EvanZJ <januar.evan2012@gmail.com>
2024-05-10 13:10:18 +0800

    feat: add saving message api

--- src/app/api/messages/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function POST(request: NextRequest){
-    console.log(request);
-    const formData = await request.formData();
-    const request_chat = formData.get("request");
-    const 
-}
