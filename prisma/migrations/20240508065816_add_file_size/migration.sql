663278278b98621dc0ed82ecf934b5912559b0db
RPW-11 <84707727+RPW-11@users.noreply.github.com>
2024-05-13 16:04:34 +0800

    Delete prisma/migrations directory

--- prisma/migrations/20240508065816_add_file_size/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `file_size` to the `Document` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Document" ADD COLUMN     "file_size" INTEGER NOT NULL;
