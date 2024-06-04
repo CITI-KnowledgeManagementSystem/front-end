62bbc1129d837b3580297553b3f196f2e9a26645
EvanZJ <januar.evan2012@gmail.com>
2024-06-04 11:58:28 +0800

    fix: model selector

--- src/lib/document-queries.ts
+++ src/lib/document-queries.ts
@@ -1,36 +1,38 @@
-"use server"
-import { prisma } from "@/db"
+"use server";
+import { prisma } from "@/db";
 
-export async function accumulatedSizeOfDocuments(id : string) {
-    try {
-        const documents = await prisma.document.groupBy({
-            by: ['userId'],
-            _sum: {
-                file_size: true,
-            },
-            where: {
-                userId: id,
-                deletedAt:null
-            },
-        });
+export async function accumulatedSizeOfDocuments(id: string) {
+  try {
+    const documents = await prisma.document.groupBy({
+      by: ["userId"],
+      _sum: {
+        file_size: true,
+      },
+      where: {
+        userId: id,
+        deletedAt: null,
+      },
+    });
 
-        const file_size= (documents[0]._sum.file_size ? documents[0]._sum.file_size / (1024 * 1024 * 1024) : 0) 
-        return Number(file_size.toFixed(2))
-    } catch (err) {
-        console.error('Error fetching documents', err);
-    }
+    if (documents.length === 0) return 0;
+    const file_size = documents[0]._sum.file_size
+      ? documents[0]._sum.file_size / (1024 * 1024 * 1024)
+      : 0;
+    return Number(file_size.toFixed(2));
+  } catch (err) {
+    console.error("Error fetching documents", err);
+  }
 }
 
-
-export async function numberOfDocuments(id : string) {
-    try {
-        const documents = await prisma.document.count({
-            where: {
-                userId: id,
-            },
-        });
-        return documents;
-    } catch (err) {
-        console.error('Error fetching documents', err);
-    }
+export async function numberOfDocuments(id: string) {
+  try {
+    const documents = await prisma.document.count({
+      where: {
+        userId: id,
+      },
+    });
+    return documents;
+  } catch (err) {
+    console.error("Error fetching documents", err);
+  }
 }
