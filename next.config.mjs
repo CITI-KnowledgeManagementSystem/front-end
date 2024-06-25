27b4a10802e63242b91e6c0f4ca040527e44b4bc
CITI-AI <kriegcom77@gmail.com>
2024-06-25 18:32:57 +0800

    installing node loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    webpack: (config, context) => {
+        config.module.rules.push({
+            test: /\.node$/,
+            loader: "node-loader",
+        });
+        return config
+    }
+};
 
 export default nextConfig;
